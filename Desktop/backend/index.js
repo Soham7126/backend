0efaede3b48524633acf843bec2cd23eee820fb8
soham7126 <76085621+sohamsawant-github@users.noreply.github.com>
2025-09-21 11:31:31 +0530

    deployment

--- Desktop/backend/index.js
+++ Desktop/backend/index.js
@@ -15,7 +15,9 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3001', 'http://localhost:3000', 'http://localhost:8080'], // Add your frontend port
+  origin: process.env.ALLOWED_ORIGINS ? 
+    process.env.ALLOWED_ORIGINS.split(',') : 
+    ['http://localhost:5173', 'http://localhost:3001', 'http://localhost:3000', 'http://localhost:8080'],
   credentials: true,
 }));
 
